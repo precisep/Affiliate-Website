d2193b576e41182920fa64fe34b409f51fb0f28c
Thabang Mokoena <thabangpardonmokoena@gmail.com>
2021-03-07 18:32:32 +0200

    initial commit

--- website/wsgi.py
+++ website/wsgi.py
@@ -10,8 +10,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+from whitenoise import WhiteNoise
+
+from affiliate import MyWSGIApp
+
+application = MyWSGIApp()
+application = WhiteNoise(application, root='/website/affiliate/Static/affiliate')
+application.add_files('/website/affiliate/Static/affiliate', prefix='more-files/')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
