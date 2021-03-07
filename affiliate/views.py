f1e9d2bd913ac608c46f25fcc6d97e3069bb37b1
Thabang Mokoena <thabangpardonmokoena@gmail.com>
2021-03-07 17:38:40 +0200

    initial commit

--- affiliate/views.py
+++ affiliate/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'affiliate/home.html',{})
+    return render(request= request,
+                  template_name = "affiliate/home.html",{})
