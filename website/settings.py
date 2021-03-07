0a67ee42be6a997a0a9f854a3cbaedaff3ac7d3d
Thabang Mokoena <thabangpardonmokoena@gmail.com>
2021-03-07 18:52:32 +0200

    initial commit

--- website/settings.py
+++ website/settings.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
@@ -111,11 +110,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR,'static'),
+#]
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static' # is equal to os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
