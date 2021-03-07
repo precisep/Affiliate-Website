403da0f7a2baf9049b32dc6450ae28c5dbe4f091
Thabang Mokoena <thabangpardonmokoena@gmail.com>
2021-03-07 19:17:20 +0200

    initial commit

--- website/settings.py
+++ website/settings.py
@@ -121,9 +121,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
