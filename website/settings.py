00ce7e07842a0612345a6c14bf3369cfa3097abf
Thabang Mokoena <thabangpardonmokoena@gmail.com>
2021-03-07 17:55:02 +0200

    initial commit

--- website/settings.py
+++ website/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['affiliate/templates'],
+        'DIRS': ['affiliate/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
